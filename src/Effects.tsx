ab83016368f126176022aed71d76d191e371d364
Daniil Zafievskiy <zafievsky@mail.ru>
2022-05-29 18:56:56 +0300

    Revert "Task done"
    This reverts commit d317523f2291587fec1df7843cd7b34c888660ad.
    
    изменено:      package-lock.json
    	изменено:      src/Effects.tsx

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,21 +1,5 @@
 import { subscribe, unsubscribe } from './resources/API';
-import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    const [lastMessage, setMessage] = useState(-1);
-    const callback = (id: number) =>{setMessage(id)};
-
-    useEffect(()=>{
-        subscribe(props.sourceId, callback);
-        return()=>{
-            setMessage(-1);
-            unsubscribe(props.sourceId, callback);
-        };
-    }, [props.sourceId]);
-    
-    return (
-        <div>
-            {props.sourceId}: {lastMessage}
-        </div>
-    )
+    return <div>123</div>;
 }
